b70480565cc7c5c79ecabc132242e507638ec0df
Jeremy Glebe <jeremyglebe@gmail.com>
2020-05-04 16:00:48 -0500

    chat setup just for testing connection

---
+++ GameServer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+clients = set()
+
+async def connection_handler(websocket, path):
+    clients.add(websocket)
+    async for message in websocket:
+        for ws in clients:
+            if ws is not websocket:
+                await ws.send(message)
+        print(message)
+    clients.remove(websocket)
+
+listen = websockets.serve(connection_handler, "localhost", 8080)
+
+asyncio.get_event_loop().run_until_complete(listen)
+asyncio.get_event_loop().run_forever()
---
+++ client/Client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+
+class Client:
+    def __init__(self, host, port):
+        self.url = "ws://{}:{}".format(host, port)
+        self.quit = False
+
+    async def start(self):
+        async with websockets.connect(self.url) as websocket:
+            messenger = asyncio.create_task(self.send(websocket))
+            listener = asyncio.create_task(self.listen(websocket))
+            await messenger
+            await listener
+        print("Disconnected!")
+
+    async def listen(self, websocket):
+        async for message in websocket:
+            print('<< {}'.format(message))
+
+    async def send(self, websocket):
+        while not self.quit:
+            message = input("> ")
+            await websocket.send(message)
+            if message == 'quit':
+                self.quit = True
+
+
+if __name__ == '__main__':
+    host = input("Enter host address: ")
+    port = input("Enter host port: ")
+    cli = Client(host, port)
+    asyncio.get_event_loop().run_until_complete(cli.start())
